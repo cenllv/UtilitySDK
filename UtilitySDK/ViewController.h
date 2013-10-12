190f955ff3a4896f5b7479ad7a8452d8bf642a97
masong2012 <474805681@qq.com>
2013-10-12 15:52:10 +0800

    Initial Commit

---
+++ UtilitySDK/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  UtilitySDK
+//
+//  Created by chujian on 13-10-12.
+//  Copyright (c) 2013年 chujian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ UtilitySDK/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  UtilitySDK
+//
+//  Created by chujian on 13-10-12.
+//  Copyright (c) 2013年 chujian. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
